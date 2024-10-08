0c92252220e1b2b92315820897025c6c40d019f1
IamJUSTaPERSONe <168302104+IamJUSTaPERSONe@users.noreply.github.com>
2024-10-08 09:59:16 +0300

    Update sistem.py

--- sistem.py
+++ sistem.py
@@ -2,6 +2,14 @@ import tkinter as tk
 from tkinter import filedialog as fd
 import ctypes
 
+
+def changeOnHover(button, colorOnHover, colorOnLeave):
+    button.bind("<Enter>", func=lambda e: button.config(
+        background=colorOnHover))
+    # background color on leving widget
+    button.bind("<Leave>", func=lambda e: button.config(
+        background=colorOnLeave))
+
 def change():
     file_path = fd.askopenfilename(filetypes=[("Image files", "*.jpg; *.jpeg;*png")])
     if file_path:
@@ -16,5 +24,7 @@ root.resizable(False, False)
 
 btn = tk.Button(root, text='Change background', command=change)
 btn.place(x= 90, y=60)
+changeOnHover(btn,  "pink", "darkred")
 
 root.mainloop()
+
