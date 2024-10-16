6f3d038f0bf67c059179849c1953eee6546200ce
IamJUSTaPERSONe <168302104+IamJUSTaPERSONe@users.noreply.github.com>
2024-10-16 19:01:41 +0300

    Add files via upload

---
+++ runButtonTkinter.py
@@ -0,0 +1,25 @@
+import tkinter as tk
+import random
+
+
+def move_button(event):
+    width = root.winfo_width()
+    height = root.winfo_height()
+
+    x = random.randint(0, width - bt.winfo_width())
+    y = random.randint(0, height - bt.winfo_height())
+
+    bt.place(x=x, y=y)
+
+
+root = tk.Tk()
+root.title('Try to catch it')
+root.geometry('400x400')
+root.resizable(False, False)
+root['background'] = '#AD66D5'
+
+bt = tk.Button(root, text='Push me if you can)', bg='#48036F', fg='white')
+bt.place(x=150, y=150)
+bt.bind('<Enter>', move_button)
+
+root.mainloop()
